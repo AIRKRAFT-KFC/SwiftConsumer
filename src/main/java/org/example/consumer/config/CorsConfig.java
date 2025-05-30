e5e8493f1ad3d2af2f0fb310f555790711c1c7fd
di0308 <di0308@naver.com>
2025-05-30 19:21:21 +0900

    fix: cors 2차 수정

--- src/main/java/org/example/consumer/config/CorsConfig.java
+++ src/main/java/org/example/consumer/config/CorsConfig.java
@@ -5,25 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-
-        // 정확한 origin 명시
-        config.addAllowedOrigin("*");
-
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-
-        config.setAllowCredentials(false);
-
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
 
